8c79db4f95e21b4afe43b61af84c4edb7f7fc5fa
supriya7768 <majajansupriya77@gmail.com>
2023-11-08 23:30:57 +0530

    Added put mapping

--- Shepherd/src/main/java/com/ts/controller/LeadFormController.java
+++ Shepherd/src/main/java/com/ts/controller/LeadFormController.java
@@ -3,13 +3,14 @@ package com.ts.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,23 +44,15 @@ public class LeadFormController {
 	    return leadData;
 	}
 	
-
-
-    @PostMapping("/updateStatus")
-    public ResponseEntity<String> updateStatus(@RequestParam Long leadId, @RequestParam String newStatus) {
-        ls.updateLeadStatus(leadId, newStatus);
-        if ("done".equals(newStatus)) {
-            ls.moveLeadToStudentPanel(leadId);
+	@PutMapping("/update-lead/{id}")
+    public ResponseEntity<String> updateLeadStatus(@PathVariable Long id, @RequestBody String selectedStatus) {
+        try {
+            // Call your service to update the lead status in the database.
+            ls.updateLeadStatus(id, selectedStatus);
+            return ResponseEntity.ok(selectedStatus);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update lead status.");
         }
-        return ResponseEntity.ok("Status updated");
     }
-    
-//    @PostMapping("/updateStatus")
-//    public String updateStatus(@RequestParam Long leadId, @RequestParam String newStatus, Model model) {
-//        // Update the status in the backend
-//
-//        // Redirect to the student panel page
-//        return "redirect:/studentpanel.html";
-//    }
 	
 }
