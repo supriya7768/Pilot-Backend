3338ff7f5c96ff2fbd51a6728e1336d2d73c526c
supriya7768 <majajansupriya77@gmail.com>
2023-11-11 14:11:16 +0530

    Added follow

--- Shepherd/src/main/java/com/ts/model/LeadForm.java
+++ Shepherd/src/main/java/com/ts/model/LeadForm.java
@@ -6,9 +6,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -32,161 +33,14 @@ public class LeadForm {
 	private int passingYear;
 	private String collegeName;
 	private String experience;
-	private int yearOfPassing;
 	private String designation;
 	private String approach;
 	private String referenceName;
 	private String batchCode;
 	private String status;
 	private String comment;
+	private String follow;
 	private String date;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCourseIntrested() {
-		return courseIntrested;
-	}
-	public void setCourseIntrested(String courseIntrested) {
-		this.courseIntrested = courseIntrested;
-	}
-	public String getMode() {
-		return mode;
-	}
-	public void setMode(String mode) {
-		this.mode = mode;
-	}
-	public String getCourseDoneInOtherInstitute() {
-		return courseDoneInOtherInstitute;
-	}
-	public void setCourseDoneInOtherInstitute(String courseDoneInOtherInstitute) {
-		this.courseDoneInOtherInstitute = courseDoneInOtherInstitute;
-	}
-	public String getInstituteName() {
-		return instituteName;
-	}
-	public void setInstituteName(String instituteName) {
-		this.instituteName = instituteName;
-	}
-	public String getReason() {
-		return reason;
-	}
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-	public String getInterest() {
-		return interest;
-	}
-	public void setInterest(String interest) {
-		this.interest = interest;
-	}
-	public String getDegree() {
-		return degree;
-	}
-	public void setDegree(String degree) {
-		this.degree = degree;
-	}
-	public String getField() {
-		return field;
-	}
-	public void setField(String field) {
-		this.field = field;
-	}
-	public int getPassingYear() {
-		return passingYear;
-	}
-	public void setPassingYear(int passingYear) {
-		this.passingYear = passingYear;
-	}
-	public String getCollegeName() {
-		return collegeName;
-	}
-	public void setCollegeName(String collegeName) {
-		this.collegeName = collegeName;
-	}
-	public String getExperience() {
-		return experience;
-	}
-	public void setExperience(String experience) {
-		this.experience = experience;
-	}
-	public int getYearOfPassing() {
-		return yearOfPassing;
-	}
-	public void setYearOfPassing(int yearOfPassing) {
-		this.yearOfPassing = yearOfPassing;
-	}
-	public String getDesignation() {
-		return designation;
-	}
-	public void setDesignation(String designation) {
-		this.designation = designation;
-	}
-	public String getApproach() {
-		return approach;
-	}
-	public void setApproach(String approach) {
-		this.approach = approach;
-	}
-	public String getReferenceName() {
-		return referenceName;
-	}
-	public void setReferenceName(String referenceName) {
-		this.referenceName = referenceName;
-	}
-	public String getBatchCode() {
-		return batchCode;
-	}
-	public void setBatchCode(String batchCode) {
-		this.batchCode = batchCode;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	
 	
 	
 	
