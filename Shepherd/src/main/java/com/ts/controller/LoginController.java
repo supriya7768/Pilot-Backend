b4b75cc07046db3ea9a17c975f8307756cfb7ebb
kirankumarpal <kiranuttampal1997@gmail.com>
2024-03-30 14:28:34 +0530

    Added Login API

--- Shepherd/src/main/java/com/ts/controller/LoginController.java
+++ Shepherd/src/main/java/com/ts/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.ts.model.Login;
 import com.ts.service.LoginService;
 import com.ts.service.OurUsersService;
 
+@CrossOrigin("*")
 @RestController
 public class LoginController {
 
